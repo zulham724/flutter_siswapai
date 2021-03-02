3a9f90a12e336e57de920a6b0560d0207715c331
anggorodhito1801 <64598384+anggorodhito1801@users.noreply.github.com>
2021-03-02 14:10:27 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -36,63 +36,109 @@ class HalamanLogin extends StatelessWidget {
                 child: Text("SISWA PAI",
                     style: TextStyle(color: Colors.white, fontSize: 30)),
               ),
-              Container(
-                width: 250,
-                height: 204,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/gambar login.png'),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/gambar login.png'),
+                    ),
                   ),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.only(left: 40, right: 40),
-                height: 313,
-                width: 360,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
-                ),
-               
+              Align(
+                alignment: Alignment.center,
+                heightFactor: 0.6,
                 child: Column(
                   children: <Widget>[
                     Container(
-                      padding: EdgeInsets.only(top: 5.0),
-                      child: Text("Selamat Datang!", style: TextStyle(fontSize: 30)),
-                    ),
-
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(labelText: "Email"),
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(labelText: "Password"),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    RaisedButton(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      color: Colors.teal[200],
-                      child: Text(
-                        "Masuk",
-                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      padding: EdgeInsets.only(left: 30, right: 30),
+                      height: 450,
+                      width: 360,
+                      margin: EdgeInsets.only(top: 30),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
                       ),
-                      onPressed: () => Get.to(() => BerandaPage()),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Row(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.only(top: 5.0),
+                            child: Text("Registrasi",
+                                style: TextStyle(fontSize: 28)),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Form(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                             Text("Email",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    )),
+                                SizedBox(
+                                  height: 5.0,
+                                ),
+                                Container(
+                                  height: 35.0,
+                                  child: TextField(
+                                    keyboardType: TextInputType.emailAddress,
+                                    decoration: new InputDecoration(
+                                        hintText: "Email",
+                                        border: OutlineInputBorder(),
+                                        contentPadding: EdgeInsets.fromLTRB(
+                                            10.0, 2.0, 5.0, 2.0)),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10.0,
+                                ),
+                                Text("Kata Sandi",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    )),
+                                SizedBox(
+                                  height: 5.0,
+                                ),
+                                Container(
+                                  height: 35.0,
+                                  child: TextField(
+                                    obscureText: true,
+                                    decoration: new InputDecoration(
+                                        hintText: "Kata Sandi",
+                                        border: OutlineInputBorder(),
+                                        contentPadding: EdgeInsets.fromLTRB(
+                                            10.0, 2.0, 5.0, 2.0)),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10.0,
+                                ),
+                              ],
+                            ),
+                          ),
+                          RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            color: Colors.teal[200],
+                            child: Text(
+                              "Daftar",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 16),
+                            ),
+                            onPressed: () => Get.to(() => BerandaPage()),
+                          ),
+                          Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
                         GestureDetector(
-                            child: Text("Buat Akun",
+                            child: Text("Belum punya akun?",
                                 style: TextStyle(
                                     decoration: TextDecoration.underline,
                                     fontSize: 16,
@@ -103,12 +149,14 @@ class HalamanLogin extends StatelessWidget {
                               style: TextStyle(
                                 decoration: TextDecoration.underline,
                                 fontSize: 16,
-                                color: Colors.black,
-                              )),
+                                color: Colors.black,)),
                           onTap: () => Get.to(() => Setting()),
                         ),
                       ],
                     )
+                  ],
+                      ),
+                    ),
                   ],
                 ),
               ),
