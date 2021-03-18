c19fed8d0d9e02ba2a9a9dd8252098c3bf835beb
Anggoro Anindhito <64598384+anggorodhito1801@users.noreply.github.com>
2021-03-18 23:42:03 +0700

    Add files via upload

--- lib/PengaturanAkun.dart
+++ lib/PengaturanAkun.dart
@@ -1,138 +1,164 @@
+import 'package:androidflutter/Beranda.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MaterialApp(
     home: PengaturanAkunPage(),
   ));
 }
-
 class PengaturanAkunPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: new Text(
-          'Pengaturan Akun',
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.teal,
-        leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.of(context).pop();
-            }),
-        iconTheme: IconThemeData(
-          color: Colors.white, //change your color here
-        ),
-      ),
-      body: Container(
-        margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10.0),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black12,
-                  spreadRadius: 0,
-                  blurRadius: 4,
-                  offset: Offset(0, 4))
-            ],
-          ),
-          height: 248.0,
-          width: 320.0,
-          padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+      backgroundColor: Colors.teal,
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
-              Form(
+              Container(
+                margin: EdgeInsets.only(
+                  top: 35.0,
+                ),
+                height: 50,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/logo_agpai.png'),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 2.0),
+                child: Text("SISWA PAI",
+                    style: TextStyle(color: Colors.white, fontSize: 30)),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/gambar login.png'),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                heightFactor: 0.6,
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(
-                      'Kata Sandi',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    SizedBox(
-                      height: 5.0,
-                    ),
-                    Text(
-                      'Password Lama',
-                      style: TextStyle(color: Colors.teal[200]),
-                    ),
-                    SizedBox(
-                      height: 3.0,
-                    ),
                     Container(
-                      height: 30.0,
-                      child: TextField(
-                        decoration: new InputDecoration(
-                            hintText: "Password lama",
-                            hintStyle: TextStyle(fontSize: 14.0),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.teal,
-                              ),
-                            ),
-                            contentPadding:
-                                EdgeInsets.fromLTRB(5.0, 2.0, 5.0, 2.0)),
+                      padding: EdgeInsets.only(left: 30, right: 30),
+                      height: 450,
+                      width: 360,
+                      margin: EdgeInsets.only(top: 30),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
                       ),
-                    ),
-                    Text(
-                      'Password Baru',
-                      style: TextStyle(color: Colors.teal[200]),
-                    ),
-                    SizedBox(
-                      height: 3.0,
-                    ),
-                    Container(
-                      height: 30.0,
-                      child: TextField(
-                        decoration: new InputDecoration(
-                            hintText: "Password Baru",
-                            hintStyle: TextStyle(fontSize: 14.0),
-                            border: OutlineInputBorder(),
-                            contentPadding:
-                                EdgeInsets.fromLTRB(5.0, 2.0, 5.0, 2.0)),
-                      ),
-                    ),
-                    Text(
-                      'Konfirmasi Password Baru',
-                      style: TextStyle(color: Colors.teal[200]),
-                    ),
-                    SizedBox(
-                      height: 3.0,
-                    ),
-                    Container(
-                      height: 30.0,
-                      child: TextField(
-                        decoration: new InputDecoration(
-                            hintText: "No. Telepon",
-                            hintStyle: TextStyle(fontSize: 14.0),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.teal,
-                              ),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.only(top: 5.0),
+                            child: Text("Pengaturan Kata Sandi",
+                                style: TextStyle(fontSize: 28)),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Form(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text("Password Lama",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    )),
+                                SizedBox(
+                                  height: 5.0,
+                                ),
+                                Container(
+                                  height: 35.0,
+                                  child: TextField(
+                                    decoration: new InputDecoration(
+                                        hintText: "Masukkan password lama anda...",
+                                        border: OutlineInputBorder(),
+                                        contentPadding: EdgeInsets.fromLTRB(
+                                            10.0, 2.0, 5.0, 2.0)),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10.0,
+                                ),
+                                Text("Password Baru",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    )),
+                                SizedBox(
+                                  height: 5.0,
+                                ),
+                                Container(
+                                  height: 35.0,
+                                  child: TextField(
+                                    keyboardType: TextInputType.emailAddress,
+                                    decoration: new InputDecoration(
+                                        hintText: "Masukkan password lama anda...",
+                                        border: OutlineInputBorder(),
+                                        contentPadding: EdgeInsets.fromLTRB(
+                                            10.0, 2.0, 5.0, 2.0)),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10.0,
+                                ),
+                                Text("Konfirmasi Password Baru",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    )),
+                                SizedBox(
+                                  height: 5.0,
+                                ),
+                                Container(
+                                  height: 35.0,
+                                  child: TextField(
+                                    obscureText: true,
+                                    decoration: new InputDecoration(
+                                        hintText: "Konfirmasi password baru anda...",
+                                        border: OutlineInputBorder(),
+                                        contentPadding: EdgeInsets.fromLTRB(
+                                            10.0, 2.0, 5.0, 2.0)),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10.0,
+                                ),
+                              ],
                             ),
-                            contentPadding:
-                                EdgeInsets.fromLTRB(5.0, 2.0, 5.0, 2.0)),
+                          ),
+                          RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            color: Colors.teal[200],
+                            child: Text(
+                              "Simpan",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 16),
+                            ),
+                            onPressed: () => Get.to(() => BerandaPage()),
+                          ),
+                                                          SizedBox(
+                                  height: 5.0,
+                                ),
+                          ],
                       ),
                     ),
                   ],
                 ),
-              ),
-              SizedBox(
-                height: 5.0,
-              ),
-              RaisedButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                color: Colors.teal[200],
-                child: Text(
-                  "Simpan",
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                ),
-                onPressed: () {},
               ),
             ],
           ),
